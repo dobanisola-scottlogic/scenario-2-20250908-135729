d7bf4ab51fcef455684b5dff489f997bb12998ed
kdavis04 <kdavis@scottlogic.com>
2023-09-06 09:16:26 +0100

    Add tests to gradle check and fix config

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { configDefaults } from 'vitest/dist/config.js';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,6 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './setupTests.ts',
+    exclude: [...configDefaults.exclude, '**/playwright/**'],
   },
 });
