0af0cd14bd2b5d2ddf367116171778dd3f8e0231
kdavis04 <110816354+kdavis04@users.noreply.github.com>
2023-09-28 11:17:22 +0100

    HAC-77 Add delete team popup (#236)
    HAC-77 Add temporary delete team popup

--- ui/src/components/dashboards/admin/HackathonDetails.tsx
+++ ui/src/components/dashboards/admin/HackathonDetails.tsx
@@ -1,13 +1,38 @@
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Button, Container, Typography } from '@mui/material';
+import { useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../../hooks';
+import {
+  selectSnackbarState,
+  setSnackbarState,
+} from '../../../slices/snackbarSlice';
+import SnackbarAlert from '../../common/SnackbarAlert';
+import DeleteTeam from '../../popups/DeleteTeam';
 
 const HackathonDetails = () => {
+  const dispatch = useAppDispatch();
+
   const { id } = useParams();
   // TODO: on HAC-75 make API call to get hackathon by id and replace the id with name in header below
+  // and move the delete team button as per UX designs
+
+  const [isDeleteTeamOpen, setIsDeleteTeamOpen] = useState(false);
+  const handleDeleteTeamOpen = () => setIsDeleteTeamOpen(true);
+
+  const snackbarState = useAppSelector(selectSnackbarState);
+  const setShowHackathonSnackbar = (isOpen: boolean) => {
+    dispatch(setSnackbarState({ isOpen, message: snackbarState.message }));
+  };
 
   return (
     <>
+      <SnackbarAlert
+        isSnackbarOpen={snackbarState.isOpen}
+        popupMessage={snackbarState.message}
+        setShowSnackbar={setShowHackathonSnackbar}
+      />
+
       <Container maxWidth={false} style={{ padding: '40px 50px' }}>
         <Box>
           <Typography
@@ -20,6 +45,12 @@ const HackathonDetails = () => {
             {id}
           </Typography>
         </Box>
+        <Button onClick={handleDeleteTeamOpen}>Delete Team</Button>
+        <DeleteTeam
+          isOpen={isDeleteTeamOpen}
+          id="5a610af8-4d9d-49d0-a927-72c43f245df9"
+          setIsOpen={setIsDeleteTeamOpen}
+        />
       </Container>
     </>
   );
