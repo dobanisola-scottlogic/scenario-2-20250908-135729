dbc70f23fcd3a309ad6a4bc352a9e837dfdd3f65
kangelSL <51117162+kangelSL@users.noreply.github.com>
2023-10-13 17:07:42 +0100

    HAC-143 Separate ARN to account name and ID (#266)
    * Separate ARN to account name and ID
    
    * Change formatting to collapse into single list
    
    * Change output to an entry for each Cloud9 URL
    
    * Use ARN and AWS name to tidy up functions

--- deployment/terraform-infrastructure/modules/contestant/outputs.tf
+++ deployment/terraform-infrastructure/modules/contestant/outputs.tf
@@ -1,11 +1,14 @@
-output "cloud9_urls" {
-  value = [for instance in aws_cloud9_environment_ec2.cloud9_instance : "https://${var.aws_region}.console.aws.amazon.com/cloud9/ide/${instance.id}"]
+data "aws_arn" "hackathon_user_arn" {
+  arn = aws_iam_user.hackathon_contestant.arn
 }
 
-output "hackathon_contestant_arn" {
-  value = aws_iam_user.hackathon_contestant.arn
-}
-
-output "hackathon_contestant_password" {
-  value = var.hackathon_contestant_password
+output "hackathon_contestants" {
+  value = [
+    for instance in aws_cloud9_environment_ec2.cloud9_instance : {
+      "Cloud9 URL" = "https://${var.aws_region}.console.aws.amazon.com/cloud9/ide/${instance.id}"
+      "Account ID": data.aws_arn.hackathon_user_arn.account
+      "Password": var.hackathon_contestant_password
+      "User name": aws_iam_user.hackathon_contestant.name
+    }
+  ]
 }
