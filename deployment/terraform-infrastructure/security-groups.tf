131808fc7e880088ea09107a55c6e7428c3f65d7
Wayne Eastaugh <48017903+weastaughscottlogic@users.noreply.github.com>
2023-10-26 15:34:33 +0100

    HAC-181 Review security groups (#289)
    * remove 5432 ingress rule from aws_db_security_group
    
    * remove 5432 ingress rule from aws_db_security_group
    
    * Extract egress rule from fargate_container_security_group to new aws_vpc_security_group_egress_rule
    
    * remove 5432 ingress rule from aws_db_security_group
    
    * Extract egress rule from fargate_container_security_group to new aws_vpc_security_group_egress_rule
    
    * Remove 8081 rule and set properties in consistent order
    
    * remove 5432 ingress rule from aws_db_security_group
    
    * Extract egress rule from fargate_container_security_group to new aws_vpc_security_group_egress_rule
    
    * Remove 8081 rule and set properties in consistent order
    
    * remove cloud9 ingress rule allowing port 80 access from anywhere on the internet
    
    * HAC-181 remove unnecessary contestant_security_group and associated rules

--- deployment/terraform-infrastructure/security-groups.tf
+++ deployment/terraform-infrastructure/security-groups.tf
@@ -4,18 +4,23 @@ resource "aws_security_group" "fargate_container_security_group" {
   description = "Access to the Fargate containers"
   vpc_id      = aws_vpc.vpc.id
 
-  # HAC-116 - Remove this if not helpful
-  # Attempting to fix ECS task error
-  # https://stackoverflow.com/a/69915083
-  egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = {
     Name = "${local.workspace}-fargate-container-security-group"
+  }
+}
+
+resource "aws_vpc_security_group_egress_rule" "fargate_to_ecr" {
+  security_group_id = aws_security_group.fargate_container_security_group.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 443
+  to_port     = 443
+  ip_protocol = "tcp"
+
+  description = "Allows Fargate container to access ECR to acquire images"
+
+  tags = {
+    Name = "${local.workspace}-fargate-to-ecr-egress-rule"
   }
 }
 
@@ -23,13 +28,6 @@ resource "aws_security_group" "aws_db_security_group" {
   name        = "${local.workspace}-aws-db-security-group"
   description = "Security group for DB access"
   vpc_id      = aws_vpc.vpc.id
-
-  ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   tags = {
     Name = "${local.workspace}-aws-db-security-group"
@@ -45,28 +43,41 @@ resource "aws_security_group" "public_load_balancer_sg" {
   }
 }
 
-resource "aws_security_group_rule" "ecs_to_db" {
-  type                     = "egress"
-  from_port                = 5432
-  to_port                  = 5432
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.fargate_container_security_group.id
-  source_security_group_id = aws_security_group.aws_db_security_group.id
+resource "aws_vpc_security_group_egress_rule" "ecs_to_db" {
+  security_group_id            = aws_security_group.fargate_container_security_group.id
+  referenced_security_group_id = aws_security_group.aws_db_security_group.id
+
+  from_port   = 5432
+  to_port     = 5432
+  ip_protocol = "tcp"
+
+  description = "Allows Fargate container to the database"
+
+  tags = {
+    Name = "${local.workspace}-fargate-to-db-egress-rule"
+  }
 }
 
-resource "aws_security_group_rule" "db_to_ecs" {
-  type                     = "ingress"
-  from_port                = 5432
-  to_port                  = 5432
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.aws_db_security_group.id
-  source_security_group_id = aws_security_group.fargate_container_security_group.id
+resource "aws_vpc_security_group_ingress_rule" "db_to_ecs" {
+  security_group_id            = aws_security_group.aws_db_security_group.id
+  referenced_security_group_id = aws_security_group.fargate_container_security_group.id
+
+  from_port   = 5432
+  to_port     = 5432
+  ip_protocol = "tcp"
+
+  description = "Allows database to be accessed by Fargate container"
+
+  tags = {
+    Name = "${local.workspace}-db-from-fargate-ingress-rule"
+  }
 }
 
 resource "aws_vpc_security_group_ingress_rule" "public_load_balancer_sg_ingress_rule" {
-  cidr_ipv4         = "0.0.0.0/0" # Allow access from anywhere on the internet
-  ip_protocol       = "-1"        # Use -1 to specify all protocols, allows traffic on all ports
   security_group_id = aws_security_group.public_load_balancer_sg.id
+
+  cidr_ipv4   = "0.0.0.0/0" # Allow access from anywhere on the internet
+  ip_protocol = "-1"        # Use -1 to specify all protocols, allows traffic on all ports
 
   tags = {
     Name = "${local.workspace}-public-load-balancer-security-group-ingress-rule"
@@ -74,11 +85,12 @@ resource "aws_vpc_security_group_ingress_rule" "public_load_balancer_sg_ingress_
 }
 
 resource "aws_vpc_security_group_egress_rule" "public_load_balancer_sg_egress_rule" {
-  cidr_ipv4         = var.vpc_cidr_block
-  from_port         = 8080
-  to_port           = 8080
-  ip_protocol       = "tcp"
   security_group_id = aws_security_group.public_load_balancer_sg.id
+
+  cidr_ipv4   = var.vpc_cidr_block
+  from_port   = 8080
+  to_port     = 8080
+  ip_protocol = "tcp"
 
   tags = {
     Name = "${local.workspace}-public-load-balancer-security-group-egress-rule"
@@ -86,10 +98,11 @@ resource "aws_vpc_security_group_egress_rule" "public_load_balancer_sg_egress_ru
 }
 
 resource "aws_vpc_security_group_ingress_rule" "ecs_security_group_ingress_from_public_alb" {
-  description                  = "Ingress from the public load balancer"
-  ip_protocol                  = -1
-  referenced_security_group_id = aws_security_group.public_load_balancer_sg.id
   security_group_id            = aws_security_group.fargate_container_security_group.id
+  referenced_security_group_id = aws_security_group.public_load_balancer_sg.id
+
+  description = "Ingress from the public load balancer"
+  ip_protocol = -1
 
   tags = {
     Name = "${local.workspace}-ecs-security-group-ingress-from-public-alb"
@@ -97,64 +110,13 @@ resource "aws_vpc_security_group_ingress_rule" "ecs_security_group_ingress_from_
 }
 
 resource "aws_vpc_security_group_ingress_rule" "ecs_security_group_ingress_from_self" {
-  description                  = "Ingress from other containers in the same security group"
-  ip_protocol                  = -1
-  referenced_security_group_id = aws_security_group.fargate_container_security_group.id
   security_group_id            = aws_security_group.fargate_container_security_group.id
+  referenced_security_group_id = aws_security_group.fargate_container_security_group.id
+
+  description = "Ingress from other containers in the same security group"
+  ip_protocol = -1
 
   tags = {
     Name = "${local.workspace}-ecs-security-group-ingress-from-self"
-  }
-}
-
-
-##################################################################################
-# Contestant resources
-##################################################################################
-# HAC-116 - Review these rules with respect to Cloud9 access;
-# Shouldn't need to allow access from anywhere on the internet
-resource "aws_security_group" "contestant_security_group" {
-  description = "Security group for the hackathon contestant instances"
-  vpc_id      = aws_vpc.vpc.id
-
-  tags = {
-    Name = "${local.workspace}-contestant-security-group"
-  }
-}
-
-resource "aws_vpc_security_group_ingress_rule" "contestant_security_group_ingress_rule_1" {
-  cidr_ipv4         = "0.0.0.0/0" # Allow access from anywhere on the internet
-  ip_protocol       = "tcp"
-  from_port         = 80
-  to_port           = 80
-  security_group_id = aws_security_group.contestant_security_group.id
-
-  tags = {
-    Name = "${local.workspace}-contestant-security-group-ingress-rule-1"
-  }
-}
-
-resource "aws_vpc_security_group_ingress_rule" "contestant_security_group_ingress_rule_2" {
-  cidr_ipv4         = "0.0.0.0/0" # Allow access from anywhere on the internet
-  ip_protocol       = "tcp"
-  from_port         = 8081
-  to_port           = 8081
-  security_group_id = aws_security_group.contestant_security_group.id
-
-  tags = {
-    Name = "${local.workspace}-contestant-security-group-ingress-rule-2"
-  }
-}
-
-resource "aws_vpc_security_group_egress_rule" "contestant_security_group_egress_rule" {
-  security_group_id = aws_security_group.contestant_security_group.id
-
-  cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 0
-  ip_protocol = "tcp"
-  to_port     = 65535
-
-  tags = {
-    Name = "${local.workspace}-contestant-security-group-egress-rule"
   }
 }
